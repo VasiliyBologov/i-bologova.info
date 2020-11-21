14f5d76424ca4468f3b00737bc22baa3b2ba5a4e
VasiliyBologov <vasiliybologov@gmail.com>
2020-11-21 05:06:59 +0200

    maintenance mod decorator

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.maintenance, name='maintenance'),
+    url(r'^$', views.main, name='main'),
 
 
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,21 +1,39 @@
 import logging
 
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+MAINTENANCE_MOD = True
+
+def maintenance(the_func):
+    """
+    Make another a function more beautiful.
+    """
+    def _decorated(*args, **kwargs):
+        if MAINTENANCE_MOD:
+            print('MAINTENANCE MOD ON')
+            return render(*args, 'maintenance.html', {})
+        else:
+            return the_func(*args, **kwargs)
+    return _decorated
 
 
-def main():
-    pass
+# @login_required(login_url='/login/')
+@maintenance
+def main(request):
+    return render(request, 'main.html', {})
 
+
+
+@maintenance
 def gallery():
     pass
 
 
-def maintenance(request):
-    return render(request, 'maintenance.html', {})
 
 def page_not_found(request, exception):
 
     logging.warning(f'page not found wtf? {str(request.get_full_path())} and ip {ip}')
     return render(request, '404.html', {})
+
+
