d623c3b295a2d9cea1d2f39a4087518205b0af63
VasiliyBologov <vasiliybologov@gmail.com>
2021-01-31 22:22:41 +0200

    add site map, fix robots

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,10 +3,19 @@ from . import views
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from gallery.sitemap import StaticViewSitemap
+
+
+sitemaps = {
+    'static': StaticViewSitemap
+}
+
 
 urlpatterns = [
     url(r'^$', views.main, name='main'),
     url(r'^gallery/$', views.gallery, name='gallery'),
+    url(r'^sitemap.xml$', sitemap, {'sitemaps': sitemaps}),
 
 ]
 
